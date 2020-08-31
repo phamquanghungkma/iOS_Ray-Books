80bdc3240fbd90b3591a43d9997e1cf58c27797b
Mikhail Dmitriev <misha.dmitriev.spb@gmail.com>
2020-08-31 18:51:25 +0300

    Delegates

--- Ray_Patterns/01_Playgrounds/FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Delegation.xcplaygroundpage/Contents.swift
+++ Ray_Patterns/01_Playgrounds/FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Delegation.xcplaygroundpage/Contents.swift
@@ -11,3 +11,47 @@
  
  ## Code Example
  */
+import UIKit
+
+public protocol MenuViewControllerDelegate: class {
+    func menuViewController(_ menuViewController: MenuViewController,
+                            didSelectItemAtIndex index: Int)
+}
+
+
+public class MenuViewController: UIViewController {
+    
+    public weak var delegate: MenuViewControllerDelegate?
+    
+    // 1
+    @IBOutlet public var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+            tableView.delegate = self }
+    }
+    // 2
+    private let items = ["Item 1", "Item 2", "Item 3"]
+}
+
+
+// MARK: - UITableViewDataSource
+
+extension MenuViewController: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            cell.textLabel?.text = items[indexPath.row]
+            return cell }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+}
+
+
+// MARK: - UITableViewDelegate
+
+extension MenuViewController: UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+         delegate?.menuViewController(self, didSelectItemAtIndex: indexPath.row)
+    }
+}
