e34b2974705d3b33134adcf740af8d1cda8ffcbf
Mikhail Dmitriev <misha.dmitriev.spb@gmail.com>
2020-09-11 15:22:53 +0300

    Facade

--- Ray_Patterns/04_MirrorPad/MirrorPad/Facades/ShareFacade.swift
+++ Ray_Patterns/04_MirrorPad/MirrorPad/Facades/ShareFacade.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2019 Razeware LLC
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,40 +28,56 @@
 
 import UIKit
 
-public class ViewController: UIViewController {
+public class ShareFacade {
 
-  // MARK: - Properties
-  public lazy var shareFacade: ShareFacade =
-    ShareFacade(entireDrawing: drawViewContainer,
-                inputDrawing: inputDrawView,
-                parentViewController: self)
-  
-  // MARK: - Outlets
-  @IBOutlet public var drawViewContainer: UIView!
-  @IBOutlet public var inputDrawView: DrawView!
-  @IBOutlet public var mirrorDrawViews: [DrawView]!
-  
-  // MARK: - View Lifecycle
-  public override func viewDidLoad() {
-    super.viewDidLoad()
-    mirrorDrawViews.forEach {
-      inputDrawView.addDelegate($0)
-    }
+  // MARK: - Instance Properties
+  public unowned var entireDrawing: UIView
+  public unowned var inputDrawing: UIView
+  public unowned var parentViewController: UIViewController
+
+  private var imageRenderer = ImageRenderer()
+
+  // MARK: - Object Lifecycle
+  public init(entireDrawing: UIView,
+              inputDrawing: UIView,
+              parentViewController: UIViewController) {
+    self.entireDrawing = entireDrawing
+    self.inputDrawing = inputDrawing
+    self.parentViewController = parentViewController
   }
 
-  // MARK: - Actions
-  @IBAction public func animatePressed(_ sender: Any) {
-    mirrorDrawViews.forEach { $0.copyLines(from: inputDrawView) }
-    mirrorDrawViews.forEach { $0.animate() }
-    inputDrawView.animate()
+  // MARK: - Facade Methods
+  public func presentShareController() {
+    let selectionViewController =
+      DrawingSelectionViewController.createInstance(
+        entireDrawing: entireDrawing,
+        inputDrawing: inputDrawing,
+        delegate: self)
+
+    parentViewController.present(selectionViewController,
+                                 animated: true)
+  }
+}
+
+// MARK: - DrawingSelectionViewControllerDelegate
+extension ShareFacade: DrawingSelectionViewControllerDelegate {
+
+  public func drawingSelectionViewControllerDidCancel(
+    _ viewController: DrawingSelectionViewController) {
+    parentViewController.dismiss(animated: true)
   }
 
-  @IBAction public func clearPressed(_ sender: Any) {
-    inputDrawView.clear()
-    mirrorDrawViews.forEach { $0.clear() }
-  }
+  public func drawingSelectionViewController(
+    _ viewController: DrawingSelectionViewController,
+    didSelectView view: UIView) {
 
-  @IBAction public func sharePressed(_ sender: Any) {
-    shareFacade.presentShareController()
+    parentViewController.dismiss(animated: false)
+    let image = imageRenderer.convertViewToImage(view)
+
+    let activityViewController = UIActivityViewController(
+      activityItems: [image],
+      applicationActivities: nil)
+    parentViewController.present(activityViewController,
+                                 animated: true)
   }
 }
