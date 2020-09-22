2ed70b3355a404e1d2e20091d0ea792b7f780a96
Mikhail Dmitriev <misha.dmitriev.spb@gmail.com>
2020-09-22 14:02:52 +0300

    Observable. Challenge

--- Ray_RxSwift/01_Playgrounds/Playgrounds/Observable.playground/Contents.swift
+++ Ray_RxSwift/01_Playgrounds/Playgrounds/Observable.playground/Contents.swift
@@ -206,6 +206,56 @@ example(of: "Single") {
     .disposed(by: disposeBag)
 }
 
+
+
+
+//MARK: - CHALLENGE 1
+
+example(of: "Challenge 1: Perform side effects") {
+    let observable = Observable<Any>.never()
+    let disposeBag = DisposeBag()
+    
+    observable
+        .do(onSubscribe: {
+            print("Subscribed")
+        })
+        .subscribe(
+            onNext: { element in
+                print(element)
+            },
+            onCompleted: {
+                print("Completed")
+            },
+            onDisposed: {
+                print("Disposed")
+            }
+        )
+        .disposed(by: disposeBag)
+}
+
+
+//MARK: - CHALLENGE 2
+
+example(of: "Challenge 2: Print debug info") {
+    let observable = Observable<Any>.never()
+    let disposeBag = DisposeBag()
+    
+    observable
+        .debug("observable")
+        .subscribe(
+            onNext: { element in
+                print(element)
+            },
+            onCompleted: {
+                print("Completed")
+            },
+            onDisposed: {
+                print("Disposed")
+            }
+        )
+        .disposed(by: disposeBag)
+}
+
 /*:
  Copyright (c) 2019 Razeware LLC
  
